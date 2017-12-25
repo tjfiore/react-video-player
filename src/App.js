1fcb4f6a426fafd9f7ce78724742a618fbe578fd
tjfiore <tjaefiore@gmail.com>
2017-12-24 23:05:42 -0500

    built searh_bar component and added its state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-const API_KEY = 'AIzaSyBB3IkIoIyyCcJukLX3qIK21PIvqN6ukms';
+const API_KEY = 'AIzaSyBB3IkIoIyyCcJukLX3qIK21PIvqN6ukms'; // youtube-api-key
 
 class App extends Component {
+  constructor(props){
+    super(props);
 
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos}); //synt sugar for this.setState({videos: videos})
+    });
+  }
 
   render() {
     return (
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class SearchBar extends Component{
+  constructor(props){
+    super(props); //calling parent method from Component
+    this.state = { //state is a plain js object used to record and react to user event.
+      term: '',
+    };
+  }
+
 
   render(){
     return(
       <div>
         <center>
-        <input type="text" className="searchBar" placeholder="Search"/>
+        <input type="text"
+        onChange={ (event) => this.setState({term : event.target.value}) } className="searchBar"
+        placeholder="Search" />
         </center>
+
       </div>
     );
   }
